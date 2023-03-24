88d5b55c24acbf15dab007e862ac80067ff2dde5
CristhianCezar <cristhiancezardev@gmail.com>
2023-03-24 16:06:24 -0300

    first commit

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+function awaitLoader() {
+    const imgLoader = document.getElementById('img-loader');
+    const container = document.getElementById('container');
+
+    imgLoader.style.display = "none";   
+    container.style.opacity = "1";
+}
